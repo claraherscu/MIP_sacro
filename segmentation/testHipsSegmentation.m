99713a58fcd18bfe1c9bcb1c9414019c5dbc4a45
claraherscu <clara.herscu@mail.huji.ac.il>
2016-11-16 17:30:17 +0200

    added test destined for hips separation
    testHipsSegmentation is a test only for separating the hips from other
    parts: should help me look into the concrete problems in manually
    identified errors in separation.

--- segmentation/testHipsSegmentation.m
+++ segmentation/testHipsSegmentation.m
@@ -1,20 +1,45 @@
 load matlabData;
 basefolder = 'sacro/dataset/';
 dataWithCanny = data;
-for i = 1:numel(dataWithCanny)
-    fPath = [basefolder, dataWithCanny{i}.accessNum];
+
+% defining a container for all the places where i identified a clear 
+% problem in hips segmentation. (containes their accessNums)
+problematicHipsSegment = {};
+% these first two are not even pelvis area
+problematicHipsSegment{end+1} = '4015005386642';
+problematicHipsSegment{end+1} = '4015005763550';
+
+
+for i = 1:numel(problematicHipsSegment)
+    fPath = [basefolder, problematicHipsSegment{i}];
     disp(fPath);
     hipsFile = [fPath '/segmentationWithCanny.mat'];
     if exist(fPath,'file') 
         display(fPath);
-
-        if exist(hipsFile,'file') > 0
-            display('Already segmented');
-        %    continue;
-        end
-        filename = [basefolder, dataWithCanny{i}.accessNum];               
+        filename = [basefolder, problematicHipsSegment{i}.accessNum];               
         tic; [ hipsSeg ] = segmentHipsAndSave(filename,'withCanny'); toc;
-        hipsFile = [fPath '/segmentationWithCanny'];
-        save(segFile, 'hipsSeg');
+%         hipsFile = [fPath '/segmentationWithCanny'];
+%         save(hipsFile, 'hipsSeg');
     end
 end
+
+    
+
+% original test code, saving for later
+% for i = 1:numel(dataWithCanny)
+%     fPath = [basefolder, dataWithCanny{i}.accessNum];
+%     disp(fPath);
+%     hipsFile = [fPath '/segmentationWithCanny.mat'];
+%     if exist(fPath,'file') 
+%         display(fPath);
+% 
+%         if exist(hipsFile,'file') > 0
+%             display('Already segmented');
+%         %    continue;
+%         end
+%         filename = [basefolder, dataWithCanny{i}.accessNum];               
+%         tic; [ hipsSeg ] = segmentHipsAndSave(filename,'withCanny'); toc;
+%         hipsFile = [fPath '/segmentationWithCanny'];
+%         save(hipsFile, 'hipsSeg');
+%     end
+% end
