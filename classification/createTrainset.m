aa6f9c5856edc5eb572a98fc0c22071a411bfa17
claraherscu <clara.herscu@mail.huji.ac.il>
2017-01-11 10:32:24 +0200

    updated Eliel's files

--- classification/createTrainset.m
+++ classification/createTrainset.m
@@ -3,32 +3,34 @@ load pointsOfInterestFull.mat
 imgMmSz = [14 14];
 imgPixlSz = round(imgMmSz/0.25);
 imgs0 = getImageFromPoint(diag0Points,imgMmSz, imgPixlSz);
-imgs0sh = getImageFromPoint(diag0Points,imgMmSz, imgPixlSz, 6);
+imgs0sh = getImageFromPoint(diag0Points,imgMmSz, imgPixlSz, [-6 -3]);
 % imgs01sh = getImageFromPoint(diag0Points,imgMmSz, imgPixlSz, 6);
 % imgs02sh = getImageFromPoint(diag0Points,imgMmSz, imgPixlSz, 6);
 % imgs03sh = getImageFromPoint(diag0Points,imgMmSz, imgPixlSz, 6);
 disp('diag1 Points');
 imgs1 = getImageFromPoint(diag1Points,imgMmSz, imgPixlSz);
-imgs1sh = getImageFromPoint(diag1Points,imgMmSz, imgPixlSz, 6);
+imgs1sh = getImageFromPoint(diag1Points,imgMmSz, imgPixlSz, [6 3]);
 % imgs12 = getImageFromPoint(diag1Points,imgMmSz, imgPixlSz, 6);
 disp('diag2 Points');
 imgs2 = getImageFromPoint(diag2Points,imgMmSz, imgPixlSz);
-imgs2sh = getImageFromPoint(diag2Points,imgMmSz, imgPixlSz,6);
+imgs2sh = getImageFromPoint(diag2Points,imgMmSz, imgPixlSz,[6 3]);
 % imgs22 = getImageFromPoint(diag2Points,imgMmSz, imgPixlSz,6);
 disp('diag3 Points');
 imgs3 = getImageFromPoint(diag3Points,imgMmSz, imgPixlSz);
-imgs3sh = getImageFromPoint(diag3Points,imgMmSz, imgPixlSz,6);
+imgs3sh = getImageFromPoint(diag3Points,imgMmSz, imgPixlSz,[6 3]);
 
 %%
-imgPixlSz = [36 36];
-imgs = {imgs0; imgs0sh; imgs1; imgs1sh; imgs2; imgs2sh; imgs3; imgs3sh};
+imgPixlSz = [21 16];
+% imgs = {imgs0; imgs0sh; imgs1; imgs1sh; imgs2; imgs2sh; imgs3; imgs3sh};
+% imgs = {imgs0; imgs0sh; imgs1; imgs1sh; imgs2; imgs2sh; imgs3; imgs3sh};
 labelsIdx = [1 1 2 2 2 2 2 2];
+% labelsIdx = [1 1 2 2 2 2];
 totalImgs = 0;
 for i = 1:numel(imgs)
     totalImgs = totalImgs + size(imgs{i},3);
 end
-X = zeros([imgPixlSz 1 totalImgs],'uint8');
-Y = zeros(2,totalImgs,'uint8');
+X = zeros([prod(imgPixlSz) totalImgs],'single');
+Y = zeros(1,totalImgs,'single');
 
 j = 1;
 for i = 1:numel(imgs)    
@@ -36,20 +38,23 @@ for i = 1:numel(imgs)
     disp(i)
     for k = 1:size(myImgs,3)
         %disp(k);
-        myImg = myImgs(:,:,k);
+        % myImg = myImgs(:,:,k);
+        myImg = myImgs(8:end-8,20:35,k);
         myImg = (myImg > 0) .* myImg;
-        X(:,:,1,j) = myImg./max(myImg(:)) .* 255;
-        Y(labelsIdx(i),j) = 1;        
+        % myImg = myImg./max(myImg(:)) .* 255;
+        X(:,j) = myImg(:);
+        Y(j) = labelsIdx(i);        
         j = j + 1;
     end
 end
 
 %% Save all images
-diagnosis = [ 0 0 1 1 2 2 3 3];
+diagnosis = [ 0 0 1 1 2 2 3 3 1 2 3];
 removeall = 0;
-folderPath = ['images/allImgs'];
-mkdir(folderPath);
 for i = 1:numel(imgs);   
+       
+    folderPath = ['images/imgDiag', num2str(diagnosis(i))];            
+    mkdir(folderPath);
     if removeall
         rmdir(folderPath);
         display(['Removed ' folderPath]);
@@ -62,11 +67,15 @@ for i = 1:numel(imgs);
     else
         label = 'P';
     end
+    
     for j = 1:size(myImgs,3);
         myImg = myImgs(:,:,j);
+        if size(myImg,1) ~= 21
+            myImg = myImgs(7:end-9,16:36,j);        
+        end
         myImg = (myImg > 0) .* myImg;
-        myImg = myImg./max(myImg(:)) .* 255;
-        imwrite(myImg, [folderPath '/img' num2str(i) '_' num2str(j) '_' label '.jpg']);
+        myImg = round(myImg./max(myImg(:)) .* 255);
+        imwrite(uint8(myImg), [folderPath '/' num2str(i) '_' num2str(j) '.png'],'png');
     end
 end
 
